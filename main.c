48894b99e63e6e195efa3201676883403a732706
sanvirg-26 <sanvi.r.g9743@gmail.com>
2026-06-06 14:13:56 +0530

    Add functions to clear and display the picture

--- main.c
+++ main.c
@@ -5,3 +5,24 @@
 #define EMPTY'_'
 #define PIXEL'*'
 char picture[HEIGHT][WIDTH];
+void clearPicture()
+{
+  for(int i=0;i<HEIGHT;i++)
+{
+for(int j=0;j<WIDTH;j++)
+{
+picture[i][j]=EMPTY;
+}
+}
+}
+void displayPicture()
+{
+  for(int i=0;i<HEIGHT;i++)
+{
+for(int j=0;j<WIDTH;j++)
+{
+printf("%c",picture[i][j]);
+}
+printf("\n");
+}
+}
